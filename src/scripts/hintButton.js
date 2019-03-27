30e938f7e2ebd5da6dd5b31ac78942ed2a681ba3
dji501 <dji501@york.ac.uk>
2019-03-27 15:55:20 +0000

    Add generic hints and update specific hint text #41

--- src/scripts/hintButton.js
+++ src/scripts/hintButton.js
@@ -13,16 +13,33 @@ export class HintButton extends React.Component {
     generateHint() {
         let computerCommitments = this.props.computerCS.totalList;
 
+        // Specific attack methods
         for (let i=0; i<computerCommitments.length; i++) {
             if (this.props.computerCS.onAssertion(computerCommitments[i]) === true) {
                 let antiprop = computerCommitments[i].denial();
                 let rule = this.checkIfCommitmentIsAttackable(antiprop);
                 if (rule !== null) {
-                    alert("'" + rule.antecedent.getContentAsString() + "' can attack '" + computerCommitments[i].getContentAsString() + "'");
+                    alert("'" + rule.antecedent.getContentAsString() + "' can be used to attack '" + computerCommitments[i].getContentAsString() + "'");
                     return;
                 }
             }
         }
+
+        // Generic advice
+        let i = Math.round((Math.random() * (2 - 0 + 1)) + 0);
+        switch(i){
+            case 0:
+                alert("Try get your opponent to agree to a point before linking it with your main argument");
+                break;
+            case 1:
+                alert("Try bring up a point that supports arguments you have already made");
+                break;
+            case 2:
+                alert("Try not to bring up topics which will ultimately weaken your position");
+                break;
+        }
+
+        return;
     }
 
     checkIfCommitmentIsAttackable(proposition) {
